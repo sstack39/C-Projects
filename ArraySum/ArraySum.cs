ab8336cb7e9f0c14f293bf452a77409e78bc8448
sstack39 <seanstack477@gmail.com>
2024-10-27 16:29:37 -0400

    Add files via upload

---
+++ ArraySum/ArraySum.cs
@@ -0,0 +1,33 @@
+﻿/*
+Name: Sean Stack
+COP2360 - Assignment 7 - Array Sum Program
+Sept 09, 2024
+Collaboration Statement: I worked on this alone
+ */
+
+
+
+using System.ComponentModel;
+
+namespace ArraySum
+{
+    internal class Program
+    {
+        //utilize static void main method for argument
+        static void Main(string[] args)
+        {
+            double cumulative = 0;
+            Random rand = new Random();
+            int[] values = new int[10];
+            //utilize for loop and System.Random.Next(100) to generate random values between 1 and 100
+            for (int i = 0; i < values.Length; i++)
+            {
+                
+                values[i] = rand.Next(100);
+                cumulative += values[i];
+                //display randomly generated number values to user as result
+                Console.WriteLine($"Value: {values[i]} Cumulative total: {cumulative} ");
+            }
+        }
+    }
+}

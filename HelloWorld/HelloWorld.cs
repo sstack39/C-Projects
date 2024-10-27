cea9d6202cbc6a56a0e520e8c381c2b9055a2ee1
sstack39 <seanstack477@gmail.com>
2024-10-27 16:28:01 -0400

    Add files via upload

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿namespace HelloWorld
+/* 
+ * Sean Stack
+ * COP 2360-1487-630
+ * Asignment 1 HelloWorld Program
+ * Date: 19 March 2024
+ * Collaboration: I referenced the professor's tutorial video as well as the textbook
+ * 
+ */
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sean");
+        }
+    }
+}
